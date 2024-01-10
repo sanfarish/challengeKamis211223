1751282de031c8889b0acd10d4aedff170b0bac1
sanfarish <farisfalah@gmail.com>
2024-01-10 21:55:48 +0700

    Third Commit

--- contoh.js
+++ contoh.js
@@ -1,10 +1,85 @@
-function sediaPayung(cuaca) {
-    if (cuaca == "hujan") {
-        payung = "sedia";
-    } else {
-        payung = "tidak sedia";
-    }
-    return payung
-}
+// let hari;
+// let tanggal = new Date().getDay();
 
-console.log(sediaPayung("cerah"));
+// function hariIni() {
+//     switch (tanggal) {
+//         case 0:
+//             hari = "ahad";
+//             break;
+//         case 1:
+//             hari = "senin";
+//             break;
+//         case 2:
+//             hari = "selasa";
+//             break;
+//         case 3:
+//             hari = "rabu";
+//             break;
+//         case 4:
+//             hari = "kemis";
+//             break;
+//         case 5:
+//             hari = "jumat";
+//             break;
+//         case 6:
+//             hari = "sabtu";
+//             break;                           
+//         default:
+//             break;
+//     }
+//     console.log(hari);
+// }
+
+// hariIni();
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i, 'Genap');
+//     } else {
+//         console.log(i, 'Ganjil');
+//     }
+// }
+
+// let arr = [2, 6, 10, 2];
+// let sum = 0;
+// for (let i = 0; i < arr.length; i++) {
+//     sum = sum + arr[i];
+// }
+// console.log(sum);
+
+// let str;
+// str = 1 + " itu ganjil";
+// console.log(str);
+
+// let i = 0;
+// while (i < 5) {
+//     console.log(i, ' tests');
+//     i++;
+// }
+
+// do {
+    
+// } while (condition);
+
+// let odd = [];
+// let even = [];
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//         even.push(i);
+//     } else {
+//         odd.push(i);
+//     }
+// }
+// console.log(odd);
+// console.log(even);
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+        console.log(i, 'FizzBuzz');
+    } else if (i % 3 == 0) {
+        console.log(i, 'Fizz');
+    } else if (i % 5 == 0) {
+        console.log(i, 'Buzz');
+    }
+}
